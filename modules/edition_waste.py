45b545a32485f2d1eadf7119241a889c414562d8
Niranjan31102 <niranjan31102@gmail.com>
2026-05-12 15:42:21 +0530

    Update edition_waste.py

--- modules/edition_waste.py
+++ modules/edition_waste.py
@@ -895,13 +895,18 @@ def render_summary_page(df, plant_name, start_date, end_date, min_date, max_date
     left_space, table_col, right_space = st.columns([0.2, 3.8, 1])
     
     with table_col:
+        styled_segment_df = round_display(segment_df).style.set_properties(
+            subset=["Waste MT"],
+            **{
+                "text-align": "center"
+            }
+        )
         st.dataframe(
-            round_display(segment_df),
+            styled_segment_df,
             use_container_width=True,
             hide_index=True,
             height=320,
         )
-
     fig_pie = px.pie(
         segment_df,
         values="Waste MT",
