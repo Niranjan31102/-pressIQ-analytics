81300c0117a61563abd7c13009b9ea35378148ba
Niranjan31102 <niranjan31102@gmail.com>
2026-05-26 20:04:30 +0530

    Update utility_performance.py

--- modules/utility_performance.py
+++ modules/utility_performance.py
@@ -1,1 +1,14 @@
+import streamlit as st
 
+def run_utility_performance_analyzer():
+
+    st.header("Utility Performance Analyzer")
+    st.caption("EMS Daily Utility Performance File Analysis")
+
+    uploaded_file = st.file_uploader(
+        "Upload EMS Daily Utility Performance File",
+        type=["xlsx", "xls", "csv"]
+    )
+
+    if uploaded_file is not None:
+        st.success("File uploaded successfully.")
